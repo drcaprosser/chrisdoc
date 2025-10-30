3a6b5d3d6fd298c117f48775b789453bcc1c7f17
Chris Prosser <chris.prosser@rhul.ac.uk>
2025-10-30 20:59:32 +0000

    versioning

--- dev/dev.R
+++ dev/dev.R
@@ -1,6 +1,11 @@
 devtools::document()
 devtools::install()
 
-devtools::install()
+# devtools::install()
 source("dev/render_example.R")
 
+devtools::build_readme()
+# usethis::use_version("dev")
+
+# usethis::use_version("minor")
+# usethis::use_version("patch")
